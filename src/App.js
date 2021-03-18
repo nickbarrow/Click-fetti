4fd2b01bc5881338b0a97b286ca467721a03f500
nickbarrow <nickb259@gmail.com>
2021-03-18 09:07:27 -0400

    Party on Wayne Party on Garth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from "react"
-import PartyButton from "./PartyButton"
-import "./styles.scss"
+import React from "react";
+import PartyButton from "./PartyButton";
+import "./styles.scss";
 
 var confettiTypes = [
-  ['🟥', '🟧', '🟨', '🟩', '🟦', '🟪', '🔴', '🟠', '🟡', '🟢', '🔵', '🟣', '🔺', '🔶', '🔷'],
-  ['🥜'],
-  ['💖', '🧡', '💛', '💚', '💙', '💜'],
-  ['💵', '💳', '💲', '🤑', '💰']
-]
+  [
+    "🟥",
+    "🟧",
+    "🟨",
+    "🟩",
+    "🟦",
+    "🟪",
+    "🔴",
+    "🟠",
+    "🟡",
+    "🟢",
+    "🔵",
+    "🟣",
+    "🔺",
+    "🔶",
+    "🔷"
+  ],
+  ["🥜"],
+  ["💖", "🧡", "💛", "💚", "💙", "💜"],
+  ["💵", "💳", "💲", "🤑", "💰"]
+];
 
 export default function App() {
   return (
     <div className="App">
-      <PartyButton text="🥳 Party on!" confetti={confettiTypes[0]} />
-      
+      <PartyButton text="🥳 Party on! Excellent!" confetti={confettiTypes[0]} />
+
       <PartyButton text="💖" confetti={confettiTypes[2]} />
 
       <PartyButton text="Add to Cart" confetti={confettiTypes[3]} />
